1843b2ec13a1d29292122113171d9f5542407d73
nlabrada <nlabrada12@gmail.com>
2018-08-09 16:28:56 -0500

    added folders and firebase

---
+++ assets/javascript.js
@@ -0,0 +1,21 @@
+// Waiting for page to load!
+$(document).ready(function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBW1ctVkn53v0VSW9WZ7q6otLyoKCkjl94",
+        authDomain: "train-scheduler-7959a.firebaseapp.com",
+        databaseURL: "https://train-scheduler-7959a.firebaseio.com",
+        projectId: "train-scheduler-7959a",
+        storageBucket: "",
+        messagingSenderId: "322988981497"
+    };
+    firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+});
